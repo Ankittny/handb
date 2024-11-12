27762795a8b46cdb36e62e9eb72d2d0b5855e10e
unknown <Santosh Pandey>
2024-11-12 15:08:24 +0530

    test fix

--- app/Http/Controllers/RestAPI/v1/auth/SocialAuthController.php
+++ app/Http/Controllers/RestAPI/v1/auth/SocialAuthController.php
@@ -41,8 +41,9 @@ class SocialAuthController extends Controller
             } elseif ($request['medium'] == 'facebook') {
                 $res = $client->request('GET', 'https://graph.facebook.com/v21.0/me?fields=id,email,name&access_token=' . $token);
                 $data = json_decode($res->getBody()->getContents(), true);
+                $dsdsds = $data['email'];
                 $emailsss = "ankittny@gmail.com";
-                dd("this is test",$data['email'],$emailsss);
+                dd("this is test",$dsdsds);
             } elseif ($request['medium'] == 'apple') {
                 $apple_login = BusinessSetting::where(['type'=>'apple_login'])->first();
                 if($apple_login){

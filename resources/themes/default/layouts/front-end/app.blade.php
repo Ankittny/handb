c290e7e802234181fbfb0c00854a918e0c215ed5
unknown <Santosh Pandey>
2025-02-26 12:59:52 +0530

    fix gtm

--- resources/themes/default/layouts/front-end/app.blade.php
+++ resources/themes/default/layouts/front-end/app.blade.php
@@ -118,7 +118,7 @@
 
     <link rel="stylesheet" href="{{theme_asset(path: 'public/assets/front-end/css/custom.css')}}">
 
-    {{--@php($google_tag_manager_id = getWebConfig(name: 'google_tag_manager_id'))
+    @php($google_tag_manager_id = getWebConfig(name: 'google_tag_manager_id'))
     @if($google_tag_manager_id )
     <!-- Google Tag Manager -->
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -135,7 +135,7 @@
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
   })(window,document,'script','dataLayer','GTM-NRPS5DT');</script>
-  <!-- End Google Tag Manager --> */  --}}
+  <!-- End Google Tag Manager --> */
 
 
     @php($pixel_analytics_user_code =getWebConfig(name: 'pixel_analytics'))
@@ -163,12 +163,12 @@
 
 <body class="toolbar-enabled">
 
-{{--@if($google_tag_manager_id)
+@if($google_tag_manager_id)
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{$google_tag_manager_id}}"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
-@endif --}}
+@endif
 
 @include('layouts.front-end.partials._modals')
 
